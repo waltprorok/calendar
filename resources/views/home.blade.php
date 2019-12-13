d962d6c5e8524ebf928cd21a570c48dd9f8eb745
Walter Prorok <wprorok@entertainmentbenefits.com>
2019-12-13 13:24:27 -0500

    updated files

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,9 +3,9 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
+        <div class="col-md-12">
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+                <div class="panel-heading">Events</div>
 
                 <div class="panel-body">
                     @if (session('status'))
@@ -14,7 +14,8 @@
                         </div>
                     @endif
 
-                    Welcome to the Event Calendar
+                    <h2>Welcome to the Event Calendar</h2>
+                    <p>Learning how to add dates and times to calendar</p>
                 </div>
             </div>
         </div>
