69b3f07274c6a57f839d3e9a0097f40cdf8d9abe
Walter Prorok <wprorok@entertainmentbenefits.com>
2019-12-13 13:36:30 -0500

    updated files

--- app/Event.php
+++ app/Event.php
@@ -7,4 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class Event extends Model
 {
     protected $fillable = ['title', 'start_date', 'end_date'];
+
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+
+    public function events()
+    {
+        return $this->hasMany('App\Event');
+    }
 }
